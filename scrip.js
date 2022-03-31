f802827b20920b126da95a319c751931f7a8c11b
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-03-31 12:46:23 -0500

    JAVASCRIPT lectura y escritura de variables css desde javascript

--- scrip.js
+++ scrip.js
@@ -1,35 +1,13 @@
 
 
-// CSSOM css object model
-// window.getComputedStyle(document.getElementById('imagen'))   --->obtener el elemento
-// window.getComputedStyle(document.getElementById('imagen')).getPropertyValue('height')---->obtener el atributo del elemento
+// escritura de variables css desde javascript para
 
-// METODO1 ACCEDIENDO A EL ELEEMTO
-document.body.style.backgroundColor='gray'
+document.documentElement.style.setProperty('--nombre','pedro_alvaro')
 
-//METODO 2---> AÑADIR CLASES A UN ELEMENTO
-document.body.classList.add('red')
-//MEDOTO 3--> CREACION DE UNA LISTA DE ESTILOS
 
-const color='yellow'
-const size=15
-const styles=`
-    background:${color};
-    color:white;
-    margin:${size}rem;
 
-`
-document.getElementById('example').setAttribute('style',styles) 
-// METODO 4-->FUNCIONE DE ESTILOS
 
-const estilos=(elemento,stiloss)=>{
-    elemento.setAttribute('style',stiloss)
-}
+// lectura de variables css desde javascript
 
-const h2=document.getElementById('h2')
-const stilosh2=`
-    background:green;
-    color:white;
-    margin:10rem;
-`
-estilos(h2,stilosh2)
+
+console.log(getComputedStyle(document.documentElement).getPropertyValue('--nombre'))
