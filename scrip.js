9f5210eb1c7ee446f2c523cd09c8d6df1edec764
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-03-31 11:13:16 -0500

    JAVASCRIPT modificando estilos desde javascript

--- scrip.js
+++ scrip.js
@@ -4,3 +4,24 @@ console.log(obtenerAnchoScroll());
 
 document.documentElement.style.setProperty('--scroll-size',obtenerAnchoScroll());
 
+
+// CSSOM css object model
+// window.getComputedStyle(document.getElementById('imagen'))   --->obtener el elemento
+// window.getComputedStyle(document.getElementById('imagen')).getPropertyValue('height')---->obtener el atributo del elemento
+
+document.body.style.backgroundColor='black'
+
+// AÑADIR CLASES A UN ELEMENTO
+document.body.classList.add('red')
+// creacion de un objeto style del
+
+const color='yellow'
+const size=15
+const styles=`
+    background:${color};
+    color:white;
+    margin:${size}rem;
+
+`
+
+document.getElementById('example').setAttribute('style',styles) 
