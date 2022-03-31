a8b7d495d54d9ad0882c80df95e8ddbca9554aeb
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-03-31 13:33:27 -0500

    JAVASCRIPT mediaquery con javascript

--- scrip.js
+++ scrip.js
@@ -1,13 +1,33 @@
+// MEDIA QUERY CON JAVASCRIPT
 
+// metodo1--->por evento
 
-// escritura de variables css desde javascript para
+addEventListener('DOMContentLoader', ()=>{
+    const puntoQuiebreMedio=matchMedia('(max-width:840px)')
+    if (puntoQuiebreMedio.matches) {
+        document.body.style.background='orange'
+    }else{
+        document.body.style.background='pink'
+    }
+})
+addEventListener('resize', ()=>{
+    const puntoQuiebreMedio=matchMedia('(max-width:840px)')
+    if (puntoQuiebreMedio.matches) {
+        document.body.style.background='orange'
+    }else{
+        document.body.style.background='pink'
+    }
+})
 
-document.documentElement.style.setProperty('--nombre','pedro_alvaro')
+// evento con funciones 
 
+const puntoQuiebreMediano=matchMedia('(max-width:540px)')
 
+const cambioColor=mediaQuery=>{
+    mediaQuery.matches
+    ?document.body.style.background='black'
+    :document.body.style.background='gray'
 
-
-// lectura de variables css desde javascript
-
-
-console.log(getComputedStyle(document.documentElement).getPropertyValue('--nombre'))
+}
+cambioColor(puntoQuiebreMediano)
+puntoQuiebreMediano.addListener(cambioColor)
